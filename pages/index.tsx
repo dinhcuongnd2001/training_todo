430902a40417d6275749f42148ef21bc710eca50
dncuong <dinhcuongnd2001@gmail.com>
2023-04-28 10:48:08 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,8 @@ import { fetchCurrId, fetchUser } from '@/redux/user.slice';
 import { debounce } from 'lodash';
 import ApiHandle from '../service';
 import { classNames } from '@/utils';
-import Pagination from '@/components/Pagination';
+// import Pagination from '@/components/Pagination';
+import { ArrowLongLeftIcon, ArrowLongRightIcon } from '@heroicons/react/20/solid';
 
 export default function Home() {
   const [filter, setFilter] = useState<string>('');
@@ -74,6 +75,21 @@ export default function Home() {
         ...rest,
       },
     });
+  };
+
+  const handlePrevious = () => {
+    const params = router.query;
+    router.push({ query: { ...params, page: +pageFilter - 1 } });
+  };
+
+  const handleNext = () => {
+    const params = router.query;
+    router.push({ query: { ...params, page: +pageFilter + 1 } });
+  };
+
+  const handleSelectPage = (x: number) => {
+    const params = router.query;
+    router.push({ query: { ...params, page: x } });
   };
 
   useEffect(() => {
@@ -195,12 +211,56 @@ export default function Home() {
           <p className=" text-sm font-medium text-gray-500">Total Pages : {totalPages}</p>
           <p className=" text-sm font-medium text-gray-500">Todos Per Page: {todosPerPage}</p>
         </div>
-        <Pagination
+        {/* <Pagination
           totalPages={Number(totalPages)}
-          currentPage={+pageFilter}
+          (+pageFilter)={+pageFilter}
           numPageShow={numPageShow}
           nearPage={nearPage}
-        />
+        /> */}
+        <nav className="w-[1300px] mt-5 flex items-center justify-between border-t border-gray-200 px-4 sm:px-0">
+          <div className="-mt-px flex w-0 flex-1">
+            <button
+              disabled={+pageFilter == 1 || totalPages == 0 ? true : false}
+              onClick={handlePrevious}
+              className="inline-flex items-center cursor-pointer border-t-2 border-transparent pr-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            >
+              <ArrowLongLeftIcon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+              Previous
+            </button>
+          </div>
+
+          <div className="hidden md:-mt-px md:flex">
+            {Number(totalPages) <= 5 ? (
+              Array.from({ length: Number(totalPages) }, (_, i) => i + 1).map((x) => (
+                <button
+                  key={x}
+                  onClick={() => handleSelectPage(x)}
+                  className={
+                    +pageFilter == x
+                      ? 'items-center border-t-2 border-gray-300 px-4 pt-4 text-sm font-medium text-gray-500'
+                      : 'cursor-pointer inline-flex items-center border-t-2 border-transparent px-4 pt-4 text-sm font-medium text-gray-500'
+                  }
+                  aria-current="page"
+                >
+                  {x}
+                </button>
+              ))
+            ) : (
+              <div></div>
+            )}
+          </div>
+
+          <div className="-mt-px flex w-0 flex-1 justify-end">
+            <button
+              disabled={+pageFilter == totalPages || totalPages == 0 ? true : false}
+              onClick={handleNext}
+              className="cursor-pointer inline-flex items-center border-t-2 border-transparent pl-1 pt-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+            >
+              Next
+              <ArrowLongRightIcon className="ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+            </button>
+          </div>
+        </nav>
       </>
 
       {openModal ? (
